81b8e9ddc2463a83b173c2592d4ebeb62d37030c
Michael Bettelyoun <michaelbettelyoun@westernwyoming.edu>
2026-09-18 00:45:28 +0000

    Add m05assignment

--- Helloworld.js
+++
@@ -1,9 +0,0 @@
-/*
-Name: Michael Bettelyoun
-Filename: helloWorld.js
-Date: September 14, 2026
-*/
-
-let hello = "Hello World";
-
-console.log(hello);
---
+++ helloworld.js
@@ -0,0 +1,26 @@
+/*
+Name: Michael Bettelyoun
+Filename: helloWorld.js
+Date: September 14, 2026
+*/
+
+let hello = "hello world";
+let number = 4.5;
+let zero = 0;
+let negativeNumber = -10;
+let isTrue = true;
+
+
+console.log(hello);
+console.log(typeof number);
+console.log(typeof zero);
+console.log(typeof negativeNumber);
+console.log(typeof isTrue); 
+
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == true);
